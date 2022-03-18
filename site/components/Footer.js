ca35f148216ab946d0750fbe8224fdb42adc5343
Brandon Unzueta <unzueta.brandon@gmail.com>
2022-03-17 21:09:27 -0700

    more basic design for index page

---
+++ site/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="w-full h-[12vh] bg-gradient-to-br from-black to-gray-800 text-gray-50 text-center py-12">
+      <p>Tim Tattooer &#169;</p>
+    </footer>
+  );
+}
--- site/pages/index.js
+++ site/pages/index.js
@@ -1,18 +1,35 @@
 import Layout from "../components/Layout";
 import HomeImage from "../components/HomeImage";
 import Image from "next/image";
+import styles from "../styles/styles.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <div
-        id="main-image"
-        className="flex text-center justify-center bg-slate-800"
-      >
-        <h1>Image goes here</h1>
+      <div className="h-[90vh] text-red-700">
+        <div
+          id="main-image"
+          className="w-full h-full relative"
+        >
+          <Image
+            src="/static/banner.jpg"
+            alt="Skull Horns"
+            layout="fill"
+            objectFit="contain"
+            quality={100}
+          />
+          <section className="absolute top-48 left-1/4 px-4 bg-gradient-to-r from-gray-100 opacity-75 rounded-2xl">
+            <h1 className="text-4xl">Tim Tattooer's Parlor</h1>
+            <h3 className="text-3xl">
+              Home of some of the <span className="text-yellow-300">BEST</span>{" "}
+              tattoos you can get in{" "}
+              <span className="text-orange-600">Orange County</span>
+            </h3>
+          </section>
+        </div>
       </div>
-      <div className="grid grid-cols-2 gap-3 justify-items-end text-white text-center pt-12 pb-10 bg-black">
-        <div className="pl-28 grid content-center">
+      <div className="grid grid-cols-1 gap-3 justify-items-end text-white text-center pt-12 pb-10 bg-black lg:grid-cols-2">
+        <div className="px-24 mb-11 lg:pl-28 2xl:pl-48 lg:mb-0 grid content-center">
           <h2 className="text-4xl">Come get your next piece here!</h2>
           <p className="text-xl pt-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -24,17 +41,16 @@ export default function Home() {
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
-        <div>
-          <div className="rounded-sm max-w-4xl">
-          <Image
-            src="/static/landing_image.jpg"
-            alt="Cool Piece"
-            height={400}
-            width={700}
-            layout="intrinsic"
-            objectFit="contain"
-            
-          />
+        <div className="mx-auto max-w-full">
+          <div className="w-80">
+            <Image
+              src="/static/landing_image.jpg"
+              alt="Cool Piece"
+              height={800}
+              width={700}
+              layout="responsive"
+              objectFit="fill"
+            />
           </div>
         </div>
       </div>

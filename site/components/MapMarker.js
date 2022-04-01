02f4c4a3ab4a4621bba444cfb97f4ce5391cc61a
Brandon Unzueta <unzueta.brandon@gmail.com>
2022-03-31 20:23:58 -0700

    adding styles to map marker

--- site/components/MapMarker.js
+++ site/components/MapMarker.js
@@ -1,52 +1,40 @@
 import React from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const apiKey = process.env.NEXT_PUBLIC_KEY;
 
-function MapMarker({}) {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: apiKey,
-  });
+const containerStyle = {
+  width: '70%',
+  height: '300px',
+  borderRadius: "20px",
+  margin: '20px',
+  position: 'relative'
+};
 
-  console.log(`The key is ${apiKey}`);
+const center = {
+  lat: 33.83,
+  lng: -117.84
+};
 
-  const containerStyle = {
-    width: "100%",
-    height: "400px",
-  };
+const marker = {
+  lat: 33.836,
+  lng: -117.8446,
+};
 
-  const center = {
-    lat: 33.836035484716284,
-    lng: -117.8446982980625,
-  };
-  
-  const [map, setMap] = React.useState(null);
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={14}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
+function MapMarker() {
+  return (
+    <LoadScript
+      googleMapsApiKey={apiKey}
     >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
-  );
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={13}
+      >
+       <Marker position={marker}/>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default React.memo(MapMarker);
+export default React.memo(MapMarker)
--- site/pages/index.js
+++ site/pages/index.js
@@ -56,7 +56,10 @@ export default function Home() {
       <QuotesCarousel slides={Quotes} />
       <div className="h-[50vh] bg-gradient-to-br from-black to-gray-800 grid grid-cols-2 text-gray-100">
         <AppointmentForm />
-        <MapMarker />
+        <div className="pl-24">
+          <h2>Come Visit Us!</h2>
+          <MapMarker />
+        </div>
       </div>
     </Layout>
   );

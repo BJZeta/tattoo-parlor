05ba150848dd0ca0782c2748d9e66bc69f54f6d3
Brandon Unzueta <unzueta.brandon@gmail.com>
2022-04-18 20:20:50 -0700

    adding styling to instagram posts

--- site/pages/gallery.js
+++ site/pages/gallery.js
@@ -8,17 +8,22 @@ export default function gallery({ instaFeed }) {
   return (
     <Layout>
       <div id="gallery" className="text-white">
-        <h1>Gallery</h1>
-        <p>{`See the latest pieces we've produced at our shop`}</p>
-        <section>
+        <h1 className="text-center text-7xl py-8 lg:text-left lg:pl-52">Gallery</h1>
+        <p className="text-center text-xl pb-8 lg:pl-52 lg:text-left">{`See the latest pieces we've produced at our shop`}</p>
+        <section className="grid grid-cols-1 gap-y-12 place-items-center lg:grid-cols-2">
           {instaFeed.map((post) => (
-            <Image
-              key={post.id}
-              src={post.media_url}
-              alt={`post: ${post.id}`}
-              height={750}
-              width={750}
-            />
+            <div className="w-[70vw] lg:w-[32vw]">
+              <Image
+                key={post.id}
+                src={post.media_url}
+                alt={`post: ${post.id}`}
+                objectFit="contain"
+                layout="responsive"
+                width={100}
+                height={100}
+                className="rounded-xl"
+              />
+            </div>
           ))}
         </section>
       </div>
@@ -27,7 +32,7 @@ export default function gallery({ instaFeed }) {
 }
 
 export async function getStaticProps() {
-  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url&access_token=${process.env.INSTA_KEY}`;
+  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=${process.env.INSTA_KEY}`;
 
   const feed = await fetch(url);
 
